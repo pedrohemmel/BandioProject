e2fcf55446847b5260fdb7e3a077c25c968de57f
pedrohemmel <phenrique_h2016@hotmail.com>
2021-12-03 11:41:10 -0300

    first commit

---
+++ app/src/main/java/br/com/local/bandioproject/SignUpActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.bandioproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_layout);
+    }
+}
